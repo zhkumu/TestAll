d2521ed0349b6f1a126e84dc463b56d919a817f7
zhkumu <zhkumu@gmail.com>
2014-11-27 17:32:38 +0800

    update end

--- src/test/mybatis/one/Test.java
+++ src/test/mybatis/one/Test.java
@@ -2,7 +2,9 @@ package test.mybatis.one;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.logging.LogFactory;
@@ -24,15 +26,21 @@ public class Test {
 	public static void main(String[] args) throws IOException   {
 		InputStream stream=Resources.getResourceAsStream("mybatis.xml");
 		SqlSessionFactory factory=new SqlSessionFactoryBuilder().build(stream);
+		
+		//打开一个不自动提交事务的会话，如果没有插入、更新、删除操作，需要调用session.commit(true)方法才会提交事务，否则在会话关闭前事务不会释放
 		SqlSession session=factory.openSession(true);
 		
 		StudentMapper mapper=session.getMapper(StudentMapper.class);
 		
-		Student student=mapper.getStudent(1);
+//		Student student=mapper.selStudentByName("mu","hehe");
+//		
+//		System.out.println(student);
 		
-		System.out.println(student);
-		
-		
+		List<Integer> ids=new ArrayList<Integer>();
+		ids.add(1);
+		ids.add(2);
+		List<Student> list=mapper.getStudents(ids);
+		System.out.println(list);
 		
 //		Student addStudent=new Student();
 //		addStudent.setName("wang1");
@@ -42,8 +50,10 @@ public class Test {
 //		System.out.println(mapper.addStudent(addStudent));
 //		
 //		session.commit();
-		
-		session.close();
+		InputStream in=System.in;
+		in.read();
+ 
+		//session.close();
 
 	}
 
--- src/test/mybatis/one/mapper/StudentMapper.java
+++ src/test/mybatis/one/mapper/StudentMapper.java
@@ -1,9 +1,23 @@
 package test.mybatis.one.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import test.mybatis.one.bean.Student;
 
 public interface StudentMapper {
 	Student getStudent(int id);
 	
 	int addStudent(Student student);
+	
+	Student selectStudent(@Param("id") int id ,@Param("name") String name);
+	
+	int updateStudent(@Param("name") String name,@Param("remark") String remark);
+
+	Student selStudent(@Param("id") int id,@Param("name") String name,@Param("remark") String remark);
+	
+	Student selStudentByName(@Param("name")String name,@Param("remark") String remark);
+	
+	List<Student> getStudents(@Param("ids") List<Integer> ids);
 }
