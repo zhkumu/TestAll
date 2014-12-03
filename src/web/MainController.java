9096844c78ff29d7cc50af69cdd976e19946c5b6
zhkumu <zhkumu@gmail.com>
2014-12-03 18:08:33 +0800

    update view

--- src/web/MainController.java
+++ src/web/MainController.java
@@ -1,8 +1,16 @@
 package web;
 
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.google.gson.Gson;
 
@@ -10,15 +18,32 @@ import com.google.gson.Gson;
 @RequestMapping("/main")
 public class MainController {
 	
-	@RequestMapping("/index")
+	//不指定为则/main对应这个方法
+	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public String index(){
 		return "index";
 	}
 	
 	@RequestMapping("/getJson")
-	@ResponseBody
-	public Object getJson(){
-		Gson gson=new Gson();
-		return gson.toJson("this is json");
+	public String getJson(Map map){
+		map.put("data", "json测试");
+		return "json";
+	}
+	
+	//上传文件
+	@RequestMapping("upload")
+	public String upload(){
+		return "upload";
+	}
+	
+	//处理文件
+	//要指定@RequestParam参数名，否则会初始化不了MultipartFile类型参数
+	@RequestMapping("saveFile")
+	public String saveFile(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException{
+		if(!file.isEmpty()){
+			File toFile=new File("D:/temp/"+file.getOriginalFilename());
+			file.transferTo(toFile);
+		}
+		return "saveFile";
 	}
 }
---
+++ src/web/view/ExcelView.java
@@ -0,0 +1,45 @@
+package web.view;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.springframework.web.servlet.view.document.AbstractExcelView;
+
+public class ExcelView extends AbstractExcelView {
+
+	@Override
+	protected void buildExcelDocument(Map<String, Object> map,
+			HSSFWorkbook hssfworkbook, HttpServletRequest httpservletrequest,
+			HttpServletResponse httpservletresponse) throws Exception {
+		HSSFWorkbook wb = new HSSFWorkbook();// 创建Excel工作簿对象
+		HSSFSheet sheet = wb.createSheet("new sheet");// 创建Excel工作表对象
+		HSSFRow titleRow = sheet.createRow(0); // 创建Excel工作表的行
+		// CellStyle cellStyle = wb.createCellStyle();// 创建单元格样式
+		// row.createCell(0)
+		// row.createCell((short) 0).setCellStyle(cellStyle); //
+		// 创建Excel工作表指定行的单元格
+		// row.createCell((short) 0).setCellValue(1); // 设置Excel工作表的值
+		List<String> titles = (List<String>) map.get("title");
+		int length=titles.size();
+		for(int i=0;i<length;i++){
+			titleRow.createCell(i).setCellValue(titles.get(i));
+		}
+		List<List<String>> datas=(List<List<String>>)map.get("data");
+		int dataLength=datas.size();
+		for(int i=1;i<dataLength;i++){
+			HSSFRow row = sheet.createRow(i);
+			List<String> list=(List<String>)datas.get(i-1);
+			for(int j=0;j<length;j++){
+				row.createCell(j).setCellValue(list.get(j));
+			}
+		}
+		
+	}
+
+}
